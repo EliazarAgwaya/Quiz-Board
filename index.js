30576d04434f6c857af394b522477cf11d056902
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-05-14 13:07:00 +0300

    Adds a switch statements to check the answer of each question

--- index.js
+++ index.js
@@ -12,4 +12,26 @@ function myScore() {
     var tenth=document.myForm.optionQTen.value;
     var total=document.getElementById('total')
     var myForm=document.getElementById('myBoard')
+    switch (score) {
+        case 0:
+        if (first=='Math.sign()'){score++};
+        case 1:
+        if (second=='Defines the document type'){score++};
+        case 2:
+        if (third=='Header'){score++};
+        case 3:
+        if (fourth=='SRC'){score++};
+        case 4:
+        if (fifth=='Logic'){score++};
+        case 5:
+        if (sixth=='Id selector'){score++};
+        case 6:
+        if (seventh=='6'){score++};
+        case 7:
+        if (eigth=='ALT'){score++};
+        case 8:
+        if (nineth=='Using a number'){score++};
+        case 9:
+        if (tenth=='Standard Input and Output'){score++};
+    }  
 }
