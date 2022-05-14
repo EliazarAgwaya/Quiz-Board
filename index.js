c10aafdc3af75fe8864b6001d5363b94080112ca
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-05-14 12:49:07 +0300

    Creates the function myScore() and declares the variables for taking the answer for each question

---
+++ index.js
@@ -0,0 +1,13 @@
+function myScore() {
+    var score=0;
+    var first=document.myForm.optionQOne.value;
+    var second=document.myForm.optionQTwo.value;
+    var third=document.myForm.optionQThree.value;
+    var fourth=document.myForm.optionQFour.value;
+    var fifth=document.myForm.optionQFive.value;
+    var sixth=document.myForm.optionQSix.value;
+    var seventh=document.myForm.optionQSeven.value;
+    var eigth=document.myForm.optionQEight.value;
+    var nineth=document.myForm.optionQNine.value;
+    var tenth=document.myForm.optionQTen.value;
+}
